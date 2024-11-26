09497b5cf8e7985a47fff01f174db8c36ba206b9
PGalva <126924139+PGalva@users.noreply.github.com>
2024-11-26 11:34:00 -0300

    Add files via upload

---
+++ Cadastro-main/Cadastro-main/api/src/main/java/med/vol/api/controller/MedicoController.java
@@ -0,0 +1,31 @@
+package med.vol.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.vol.api.medico.DadosCadastrosMedico;
+import med.vol.api.medico.DadosListagemMedico;
+import med.vol.api.medico.Medico;
+import med.vol.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("medicos")
+@CrossOrigin(origins = "http://localhost:8080/medicos", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastrosMedico dados) {
+        repository.save(new Medico(dados));
+    }
+@GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+}
